31b9057f54a27715dba51b78a9347d588d33b376
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 03:33:33 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,18 @@
 require 'pry'
 def hello_t(names)
     #binding.pry
-  names.each {|name| puts name}
+ # names.each {|name| puts name}
    #names.each {|name| yield(name)}
-  
+   
+  i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
+
 end
  
 # call your method here!
