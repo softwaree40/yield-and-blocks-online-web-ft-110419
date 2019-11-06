7f15be8ece5a94721f6d4daa65c41500821b2504
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-06 03:41:06 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,23 @@
-def hello_t
-
+require 'pry'
+#def hello_t(array)
+    #binding.pry
+ # names.each {|name| puts name}
+   #names.each {|name| yield(name)}
+   
+  def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
-
+ 
 # call your method here!
 
